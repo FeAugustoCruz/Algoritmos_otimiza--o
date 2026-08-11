a797d6480705c159987b794913c864254ae32f22
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2026-08-11 17:46:39 -0300

    1ª Aula

---
+++ exemplo_flag.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//goto
+//Uso de flags
+
+int main(void){
+    int flag = 0;
+    for(int i = 0; i < 10; i ++){
+        for(int j = 0; j < 20; i ++){
+
+            
+        }
+    }
+
+    goto FIM;
+
+
+    FIM: ;
+
+
+    return 0;
+}
