e53e3a5bd0280b483f283f0ab829c765ca40277d
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2026-09-06 23:31:19 -0300

    pab feito

--- pab.cpp
+++ pab.cpp
@@ -3,25 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <iostream>
 
 int main(void){
     srand(time(NULL));
     char arq[50];
     Solucao solucao;
-
+    memset(&solucao, 0, sizeof(Solucao));
 
     strcpy(arq, "i01.txt");
     ler_dados(arq);
-
     for(int i = 0; i < num_berco; i ++){
-        for(int j = 0; i < num_navio; j ++){
+        solucao.qtd_berco[i] = rand() % num_berco;
+        for(int j = 0; j < num_navio; j ++){
             solucao.t_atracacao[i][j] = rand() % MAX_NAVIOS;
         }
     }
 
-    strcpy(arq, "");
-    testar_dados(arq);
 
+    strcpy(arq, "");
+    //testar_dados(arq);
+    calcular_FO(solucao);
+    escrever_FO(solucao);
     return 0;
 }
 
@@ -86,15 +89,55 @@ int testar_dados(char* arq){
     if(strcmp(arq, "") != 0){
         fclose(f);
     }   
-
 }
   
-int calcular_FO(Solucao& s){
+void calcular_FO(Solucao& s){
     s.fo = 0;
-    for(int b = 0;b < ; b++){
-        int hora = temp_chegada[];
-        for(){
+    
+    for(int b = 0; b < num_berco; b++){
+        int hora = aber_berco[b];
 
+        for(int i = 0; i < s.qtd_berco[b]; i++){
+            int navio = s.t_atracacao[b][i];
+
+            // Proteção: Berço incompatível (Tempo = 0)
+            if(temp_atedimento[b][navio] == 0) {
+                s.fo += 100000; // Penalidade pesada por atracação impossível
+                continue;
+            }
+
+            if(temp_chegada[navio] > hora){
+                hora = temp_chegada[navio];
+            }
+            
+            // CORREÇÃO: Matriz acessada na ordem correta [berço][navio]
+            hora += temp_atedimento[b][navio];
+            
+            // Adiciona o tempo de serviço daquele navio à FO total
+            s.fo += hora - temp_chegada[navio];
+            
+            // CORREÇÃO: Subtração usando temp_saida, sem duplicação de MAX()
+            if(hora > temp_saida[navio]){
+                s.fo += PES_PRAZO_NAV * (hora - temp_saida[navio]);
+            }
+        }
+        
+        // CORREÇÃO: Apenas um check simples para fechamento, sem duplicação de MAX()
+        if(hora > fecha_berco[b]){
+            s.fo += PES_FEC_BER * (hora - fecha_berco[b]);
         }
     }
 }
+
+void escrever_FO(Solucao& s){
+    printf("Funcao Objetivo: %d\n", s.fo);
+    printf("Matriz de Atracacao:\n");
+
+    for(int i = 0; i < num_berco; i++){
+        for(int j = 0; j < s.qtd_berco[i]; j ++){
+            printf("[%d] ", s.t_atracacao[i][j]);
+        }
+        printf("\n");
+    }
+}
+
--- pab.h
+++ pab.h
@@ -1,11 +1,16 @@
 #define MAX_NAVIOS 60
 #define MAX_BERCOS 13
 
+#define PES_FEC_BER 5
+#define PES_PRAZO_NAV 5
+
+#define MAX(X, Y) ((X > Y) ? X : Y)
+
 typedef struct fSolucao
 {
     int fo;
     int t_atracacao[MAX_BERCOS][MAX_NAVIOS];
-    int qtd_berco[MAX_NAVIOS];
+    int qtd_berco[MAX_BERCOS];
 }Solucao;
 
 
@@ -21,4 +26,5 @@ int temp_saida[MAX_NAVIOS];
 
 int ler_dados(char* arq);
 int testar_dados(char* arq);
-int calcular_FO(Solucao& s);
+void calcular_FO(Solucao& s);
+void escrever_FO(Solucao& s);
